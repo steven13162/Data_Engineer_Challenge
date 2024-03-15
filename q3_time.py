a536399c0f87f717c26d6b4c4f0a66acedf047ce
Steven <steven13162@gmail.com>
2024-03-15 08:56:03 -0500

    feat: adjust read JSON

--- q3_time.py
+++ q3_time.py
@@ -3,12 +3,24 @@ import pandas as pd
 import numpy as np
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    dynamic_key = 'username'
-    df = pd.read_json(file_path, lines=True)
 
-    #Ejercicio N#3
-    df[dynamic_key + '_'] = df['user'].apply(lambda x: x.get(dynamic_key))
-    df['conteo_menciones'] = df['mentionedUsers'].str.len()
-    df['conteo_menciones'] = np.where(np.isnan(df['conteo_menciones']), 0, df['conteo_menciones'])
+    #Parametros Iniciales
+    dynamic_key = 'username'
+    chunksize = 10000
+    columnas_lectura = ['user','mentionedUsers']
+    chunks = []
+
+    #Lectura de JSON por bloques
+    for chunk in pd.read_json(file_path, lines=True, chunksize=chunksize):
+        #Filtrato de info necesaria
+        chunk=chunk[columnas_lectura]  
+        chunk[dynamic_key + '_'] = chunk['user'].apply(lambda x: x.get(dynamic_key))
+        chunk['conteo_menciones'] = chunk['mentionedUsers'].str.len()
+        chunk['conteo_menciones'] = np.where(np.isnan(chunk['conteo_menciones']), 0, chunk['conteo_menciones'])
+
+        chunks.append(chunk)
+        
+    df = pd.concat(chunks,ignore_index=True)
+
     usuarios_menciones = df.groupby(['username_'])['conteo_menciones'].sum().reset_index(name='#_menciones').sort_values(by='#_menciones', ascending=False).head(n=10)
     return usuarios_menciones.values
